1fa5c403b4b63cb79bdee3dbb10d7da61a094750
Soumik <soumik@DeviceNotFound>
2020-10-24 03:27:10 +0530

    Updates

---
+++ precache-manifest.ce4d7e9204d768f21e987478ebaded11.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1737528518fc51d7340c8e432aeb913f",
+    "url": "/Soumi-Sen/index.html"
+  },
+  {
+    "revision": "605d7ac0669ab95f89f8",
+    "url": "/Soumi-Sen/static/css/main.6d45ec53.chunk.css"
+  },
+  {
+    "revision": "64ec19dc6b839b0e4bae",
+    "url": "/Soumi-Sen/static/js/2.75043c58.chunk.js"
+  },
+  {
+    "revision": "ee2f0ebdf321a3fcb47efd5ede80e917",
+    "url": "/Soumi-Sen/static/js/2.75043c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "605d7ac0669ab95f89f8",
+    "url": "/Soumi-Sen/static/js/main.e8cfdbf6.chunk.js"
+  },
+  {
+    "revision": "4c4a59869143f3185727",
+    "url": "/Soumi-Sen/static/js/runtime-main.ba594288.js"
+  },
+  {
+    "revision": "b42b834aa667cbedd504edfb615289b0",
+    "url": "/Soumi-Sen/static/media/bg4.b42b834a.jpg"
+  },
+  {
+    "revision": "0b0c6b6db1566d93654d47b0d79fd07f",
+    "url": "/Soumi-Sen/static/media/code.0b0c6b6d.jpg"
+  },
+  {
+    "revision": "2f1ccc5ff7743254806f0f88e84c9aec",
+    "url": "/Soumi-Sen/static/media/sunset2.2f1ccc5f.jpg"
+  },
+  {
+    "revision": "fb2474a8e5020506d58e7b88f80a4118",
+    "url": "/Soumi-Sen/static/media/thobra.fb2474a8.jpg"
+  }
+]);
